21b684648dbd07e1c0edbbff29efddc6bb2c07c4
jeveriyamukul <mukul.jeveriya@bytelearn.ai>
2022-10-02 21:24:56 +0530

    adding files

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import time
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/times")
+def times():
+    t=time.localtime()
+    current_time = time.strftime("%H:%M:%S", t)
+    return current_time
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0')
